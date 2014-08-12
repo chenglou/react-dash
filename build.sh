2bb8f2692ab3f936e944c3359134c3c85b75823a
Cheng Lou <chenglou92@gmail.com>
2014-08-12 14:51:32 -0700

    Fix sqlite creation

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 mkdir -p Contents/Resources/Documents
+# create a fresh sqlite db
+cd Contents/Resources
+sqlite3 docSet.dsidx 'CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT)'
+sqlite3 docSet.dsidx 'CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path)'
 # fetch the whole doc site
-cd Contents/Resources/Documents
+cd Documents
 wget -m -p -E -k -np http://facebook.github.io/react/
 # move it around a bit
 mv facebook.github.io/react ./
